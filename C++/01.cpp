5d9f070da52ca7ba8ca24a184574f93f83d95ef7
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-09-21 01:49:25 +0600

    Say Hellow World With C++

---
+++ C++/01.cpp
@@ -0,0 +1,10 @@
+//Problem name: Say "Hello, World!" With C++
+//https://www.hackerrank.com/challenges/cpp-hello-world/problem?isFullScreen=true
+
+#include<iostream>
+using namespace std;
+int main(){
+    string a = "Hello, World!";
+    cout << a << endl;
+    return 0;
+}
