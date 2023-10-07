9124cff46bc7b299e9fc9c6d5cda100bf6d5f4f7
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-10-08 02:04:05 +0600

    Basic Data Types

---
+++ C++/02.cpp
@@ -0,0 +1,19 @@
+//Problem name: Basic Data Types
+//https://www.hackerrank.com/challenges/c-tutorial-basic-data-types/problem?isFullScreen=true
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    scanf("%d %ld %c %f %lf", &a, &b, &c, &d, &e);
+    printf("%d\n%ld\n%c\n%f\n%lf\n", a, b, c, d, e);
+
+    return 0;
+}
+
+
